a0c7125843e402e4822cc80765874b117d0b29c4
Colby Workman <cgw2319@gmail.com>
2021-11-08 16:06:25 -0700

    delete.sql done

--- delete.sql
+++ delete.sql
@@ -1,10 +1,19 @@
--- INSERT INTO animals ( name, type, age ) 
--- VALUES ('Leo', 'lion', 12),
--- ('Jerry', 'mouse', 4),
--- ('Marty', 'zebra', 10),
--- ('Gloria', 'hippo', 8),
--- ('Alex', 'lion', 9),
--- ('Melman', 'giraffe', 15),
--- ('Nala', 'lion', 2),
--- ('Marie', 'cat', 1),
--- ('Flounder', 'fish', 8);
+INSERT INTO animal ( name, type, age ) 
+VALUES ('Leo', 'lion', 12),
+('Jerry', 'mouse', 4),
+('Marty', 'zebra', 10),
+('Gloria', 'hippo', 8),
+('Alex', 'lion', 9),
+('Melman', 'giraffe', 15),
+('Nala', 'lion', 2),
+('Marie', 'cat', 1),
+('Flounder', 'fish', 8);
+
+DELETE FROM animal
+WHERE type = 'lion';
+
+DELETE FROM animal
+WHERE name LIKE 'M%';
+
+DELETE FROM animal
+WHERE age < 9;
